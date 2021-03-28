d2af8b43ae4d5299118e93bc43deab9337d62645
NihadZeidan <nihadzeidan@gmail.com>
2021-03-28 17:32:09 +0300

    feature 3

--- server.js
+++ server.js
@@ -16,8 +16,19 @@ app.use(cors());
 
 
 // This is the Routes to find the files and get data from them 
-app.get('/location', getLocation)
-    // app.get('/', () => console.log("WELCOME!"));
+app.get('/location', getLocation);
+// app.get('/', () => console.log("WELCOME!"));
+app.get('/weather', getWeather);
+
+
+function getWeather(request, response) {
+    const searchQuery = request.query;
+
+    let weatherData = require("./data/weather.json");
+    let eachWeather = new WeatherDataToFit(weatherData.data[0]);
+
+    response.send(eachWeather);
+}
 
 function getLocation(request, response) {
     const searchQuery = request.query;
@@ -28,13 +39,20 @@ function getLocation(request, response) {
 
 }
 
-
+//  Constractor functions to fit the data with the frontEnd
 function LocationDataToFit(data) {
     this.formatted_query = data.display_name;
     this.latitude = data.lat;
     this.longitude = data.lon;
     this.search_query = data.type
 }
+
+function WeatherDataToFit(day) {
+    this.forecast = day.weather.description
+    this.time = day.datetime
+}
+
+
 
 
 //  This to Listen to your port when your run it !
